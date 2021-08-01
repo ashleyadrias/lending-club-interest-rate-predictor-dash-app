3c045c2a38e0d7e33165e36549e584df46cc021a
smash <smash@smashs-Mac-mini.local>
2021-08-01 13:43:06 -0700

    add file'

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# lending-club-interest-rate-predictor-dash-app
+# Interest Rate Predictorusing Lending Club Loan Data
+
+By Ashley Adrias
+
+Interested in knowing the interest rate on your loan type without running a credit check? 
+
+Look no further!
+
+Use this interest rate predictor app to see how much your interest rate will be based on your 
+annual income, credit score, loan amount, loan purpose, Debt Utilization %, and Debt-to-Income %.
+
+Visit: <>
---
+++ pages/predictions.py
@@ -0,0 +1,141 @@
+import dash
+import dash_bootstrap_components as dbc
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+from joblib import load
+from app import app
+import pandas as pd
+import numpy as np
+
+
+pipeline = load('notebooks/pipeline.joblib')
+
+column1 = dbc.Col(
+    [
+        dcc.Markdown(
+            """
+        
+            ## Predictions
+
+
+            """, className='mb-5'
+        ),
+        dcc.Markdown('#### Annual Income'),
+        dcc.Slider(
+            id='annual_inc',
+            min=20000, 
+            max=200000, 
+            step=5, 
+            value=220000, 
+            marks={n: str(n) for n in range(20000,220000,20000)},
+            className='mb-5', 
+        ),
+        dcc.Markdown('#### Credit Score'),
+        dcc.Slider(
+            id='fico_range_low',
+            min=650, 
+            max=850, 
+            step=5, 
+            value=900, 
+            marks={n: str(n) for n in range(650,900,50)},
+            className='mb-5',
+        ),
+        dcc.Markdown('#### Loan Amount'),
+        dcc.Slider(
+            id='loan_amnt',
+            min=5000, 
+            max=40000, 
+            step=5, 
+            value=45000, 
+            marks={n: str(n) for n in range(5000,45000,5000)},
+            className='mb-5',           
+        ),
+        dcc.Markdown('#### Loan Purpose'), 
+        dcc.Dropdown(
+            id='purpose',
+            options = [
+                {'label': 'debt_consolidation', 'value': 'debt_consolidation'}, 
+                {'label': 'credit_card', 'value': 'credit_card'}, 
+                {'label': 'home_improvement', 'value': 'home_improvement'}, 
+                {'label': 'other', 'value': 'other'}, 
+                {'label': 'car', 'value': 'car'}, 
+                {'label': 'major_purchase', 'value': 'major_purchase'}, 
+                {'label': 'educational', 'value': 'educational'}, 
+                {'label': 'vacation', 'value': 'vacation'}, 
+                {'label': 'moving', 'value': 'moving'},
+                {'label': 'medical', 'value': 'medical'},
+                {'label': 'house', 'value': 'house'},
+                {'label': 'wedding', 'value': 'wedding'}, 
+                {'label': 'small_business', 'value': 'small_business'}, 
+                {'label': 'renewable_energy', 'value': 'renewable_energy'}, 
+            ],
+            value = 'debt_consolidation',
+            className='mb-5',
+
+        ),
+        dcc.Markdown('#### term'), 
+        dcc.Dropdown(
+            id='term',
+            options = [
+                {'label': '36 months', 'value': '36 months'}, 
+                {'label': '60 months', 'value': '60 months'},
+            ],
+            value = '36 months',
+            className='mb-5',
+
+        ),
+        dcc.Markdown('#### Revolving Credit Debt(%)'),
+        dcc.Slider(
+            id='revol_util',
+            min=0, 
+            max=100, 
+            step=5, 
+            value=100, 
+            marks={n: str(n) for n in range(0,110,10)},
+            className='mb-5',
+
+        ),
+        dcc.Markdown('#### Debt-to-Income(%)'),
+        dcc.Slider(
+            id='dti',
+            min=0, 
+            max=100, 
+            step=5, 
+            value=100, 
+            marks={n: str(n) for n in range(0,110,10)},
+            className='mb-5',
+
+        ),
+        
+    ],
+    md=7,
+)
+
+column2 = dbc.Col(
+    [
+        html.H2('Expected Interest', className='mb-5'), 
+        html.Div(id='prediction-content', className='lead'),
+        html.Img(src='assets/calculator_money.jpeg', className='img-fluid')
+    ]
+)
+
+layout = dbc.Row([column1, column2])
+
+
+@app.callback(
+    Output('prediction-content', 'children'),
+    [Input('fico_range_low', 'value'), Input('term', 'value'),Input('dti', 'value'),Input('revol_util', 'value'),Input('annual_inc', 'value'),Input('loan_amnt', 'value'),Input('purpose', 'value')],
+)
+def predict(fico_range_low, term, dti, revol_util, annual_inc,loan_amnt,purpose):
+    df = pd.DataFrame(
+        columns=["fico_range_low", 'term', 'dti', 'revol_util', 'annual_inc', 'loan_amnt', 'purpose'], 
+        data=[[fico_range_low, term, dti, revol_util, annual_inc,loan_amnt,purpose]]
+    )
+    y_pred = pipeline.predict(df)[0]
+    return f'{y_pred:.2f}% Interest Rate'
+
+#Check data types: f"{fico_range_low} {type(fico_range_low)}, {term} {type(term)}, {dti}{type(dti)}, {revol_util}{type(revol_util)}, {annual_inc}{type(annual_inc)},{loan_amnt}{type(loan_amnt)},{purpose}{type(purpose)}"
+
+
+
